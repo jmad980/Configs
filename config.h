b5a41c930bbb0d04e9d9063d41af311b8a47dd8a
jmad980 <jmad980@gmail.com>
2010-10-02 11:18:46 -0700

    color changes

--- config.h
+++ config.h
@@ -4,11 +4,11 @@
 /* appearance */
 static const char font[]            = "-*-terminus-medium-r-*-*-16-*-*-*-*-*-*-*";
 static const char normbordercolor[] = "#cccccc";
-static const char normbgcolor[]     = "#cccccc";
-static const char normfgcolor[]     = "#000000";
+static const char normbgcolor[]     = "#000000";
+static const char normfgcolor[]     = "#0027FF";
 static const char selbordercolor[]  = "#0066ff";
-static const char selbgcolor[]      = "#0066ff";
-static const char selfgcolor[]      = "#ffffff";
+static const char selbgcolor[]      = "#00000F";
+static const char selfgcolor[]      = "#0027FF";
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const Bool showbar           = True;     /* False means no bar */
@@ -47,7 +47,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "uxterm", NULL };
+static const char *termcmd[]  = { "terminal", NULL };
  /* Volume */
 static const char *sound_up[] = { "/usr/bin/amixer", "-q", "set", "Master", "3+", NULL };
 static const char *sound_down[] = { "/usr/bin/amixer", "-q", "set", "Master", "3-", NULL };
